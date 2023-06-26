d92857d813136db7d40ae238d720b60a45aab433
Sang Shin <sangshinpassion@gmail.com>
2023-06-26 14:09:12 -0400

    change

--- src/main/java/com/perscholas/model/Address.java
+++ src/main/java/com/perscholas/model/Address.java
@@ -9,7 +9,7 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(sx)
+@Table()
 public class Address implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
